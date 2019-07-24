fae2f845534bd5f2aeab3afae25808eced208a16
Aman Sinha <aman@simpalm.com>
2019-07-24 20:34:35 +0530

    Updated framework for making it compatible with Swift 5.0 compiler

---
+++ CustomDialogSimpalm/CustomSimpalmDialog.framework/Headers/CustomSimpalmDialog.h
@@ -0,0 +1,19 @@
+//
+//  CustomSimpalmDialog.h
+//  CustomSimpalmDialog
+//
+//  Created by Aman Sinha on 24/07/19.
+//  Copyright © 2019 Simpalm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CustomSimpalmDialog.
+FOUNDATION_EXPORT double CustomSimpalmDialogVersionNumber;
+
+//! Project version string for CustomSimpalmDialog.
+FOUNDATION_EXPORT const unsigned char CustomSimpalmDialogVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomSimpalmDialog/PublicHeader.h>
+
+
